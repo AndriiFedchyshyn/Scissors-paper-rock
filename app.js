822dc4c83523f7ef599bb2cf12de942877bfa0df
Andrii Fedchyshyn <126354421+AndriiFedchyshyn@users.noreply.github.com>
2025-04-05 21:55:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ buttons.forEach((button, i) => {
   button.addEventListener('click', (e) => {
     const userChoice = i;
     const computerChoice = Math.floor(Math.random() * 3);
-    window.location.href = `/result.html?user=${userChoice}&computer=${computerChoice}`;
+    window.location.href = `result.html?user=${userChoice}&computer=${computerChoice}`;
     })
 })
